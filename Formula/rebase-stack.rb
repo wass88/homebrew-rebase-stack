e1bfef44b46a160fa27e11602239954d459f8f94
wass88 <wasss80@gmail.com>
2025-05-07 19:13:43 +0900

    rebase-stack 0.1.7
    Created by https://github.com/mislav/bump-homebrew-formula-action

--- Formula/rebase-stack.rb
+++ Formula/rebase-stack.rb
@@ -1,8 +1,8 @@
 class RebaseStack < Formula
   desc      "Automated stacked Git rebase helper"
   homepage  "https://github.com/wass88/rebase-stack"
-  url       "https://github.com/wass88/rebase-stack/archive/v0.1.0.tar.gz"
-  sha256    "wip"
+  url       "https://github.com/wass88/rebase-stack/archive/refs/tags/v0.1.7.tar.gz"
+  sha256    "44920711effd59f27ecc22c94a1b1e1cd65a09afe744304ff7b063b3cfc5a2c4"
   license   "MIT"
 
   def install
